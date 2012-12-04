74379b8a959e7a682580651f73637e54102416c3
Naoto Takai <takai@cookpad.com>
2012-12-04 17:15:04 +0900

    Add support for default port

--- lib/foreman/kicker.rb
+++ lib/foreman/kicker.rb
@@ -4,7 +4,21 @@ require "foreman/kicker/watchdog"
 
 module Foreman
   module Kicker
+    def self.port=(port)
+      @port = port
+    end
+
+    def self.port
+      @port
+    end
+
     def self.kick(*args)
+      if args.include?('-p') || args.include?('--port')
+        self.port = args[(args.find('-p') || args.find('--port')) + 1]
+      else
+        args += ['-p', port]
+      end
+
       @watchdog = Foreman::Kicker::Watchdog.new
       @watchdog.start(*args)
     end
